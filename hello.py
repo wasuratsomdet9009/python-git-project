c53240c6555c1402dbc9bffdf3c8460978a5c21a
wasuratsomdet0217 <89716712+wasuratsomdet9009@users.noreply.github.com>
2025-08-09 12:09:10 +0700

    feat: add farewell function

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import datetime
 import config
 
 def say_hello(name):
-   print(f"Hello, {name} from {config.APP_NAME}!")
+      print(f"Goodbye, {name} from {config.APP_NAME}!")
 
 def greet_user():
    name = input("Please enter your name: ")
